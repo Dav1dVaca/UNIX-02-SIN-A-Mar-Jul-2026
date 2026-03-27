12547f668cb9d8bcb7335485861cd979be238b67
Dav1dVaca <davacagu@uide.edu.ec>
2026-03-27 17:14:46 +0000

    saludobin.

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -3,3 +3,14 @@ ls      # list files and directoyes on the / dev directory
 cd /proc # change directory / proc directory
 ls       # list files and directoyes on the / dev directory
 cat cpuinfo # give me the cpu info
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/ # take me to the original one
+ls  # list files and directoyes on the / dev directory
+gcc saludo.c -o saludobin # compiler the file
+sudo mv saludobin /bin/  # move saludobin a /bin
+ls /bin | grep "saludo*" # prove if the files move
+cd # change directory
+saludobin # run code
+cd /etc # change directory / etc
+ls     # list files and directoyes on the / dev directory
+cd gss/ # change directory / gss
+saludobin # run code
