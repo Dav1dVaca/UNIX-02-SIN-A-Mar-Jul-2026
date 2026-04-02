74db88b1636105fb1c24d4d73e259bb927a5fb87
Dav1dVaca <davacagu@uide.edu.ec>
2026-04-02 12:53:16 +0000

    parted

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -1,2 +1,5 @@
 cd / # change to the main directory
 ls -F # lists files
+sudo apt update # update the package list
+sudo apt upgrade # upgrade all the installed packages to the latest version
+sudo apt install parted # install parted
