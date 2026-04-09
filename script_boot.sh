d7c25c67d7f10b8cf5f6ea3a77762dadf86cb46d
Dav1dVaca <davacagu@uide.edu.ec>
2026-04-09 12:25:26 +0000

    routes to directories

---
+++ script_boot.sh
@@ -0,0 +1,5 @@
+mkdir proyecto # create a directory "proyecto"
+ls # list the directories 
+cd proyect/ # use a alternative route to move
+cd .. # move to the principal directorie
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto # use a absolute route to move
