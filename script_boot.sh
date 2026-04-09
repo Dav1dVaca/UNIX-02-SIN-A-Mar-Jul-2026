dfb6f20e3cc4b550e669bc6633935a47787b2b4c
Dav1dVaca <davacagu@uide.edu.ec>
2026-04-09 12:52:19 +0000

    4 change d

--- script_boot.sh
+++ script_boot.sh
@@ -3,3 +3,11 @@ ls # list the directories
 cd proyect/ # use a alternative route to move
 cd .. # move to the principal directorie
 cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto # use a absolute route to move
+pwd # print working directory
+cd  # go to the home directory
+cd /home/codespace # go to the home directory
+cd ~ # go to the home directory
+cd $HOME # go to the home directory
+echo $BASH # print bash variable
+echo $PATH # print the contents of the environment variable to the screen
+cd /usr/local # move from your current folder to the system folder called /usr/local
