001977326916f695347b0412b5729741386dac37
Dav1dVaca <davacagu@uide.edu.ec>
2026-04-09 13:13:01 +0000

    ls lai

--- script_boot.sh
+++ script_boot.sh
@@ -11,3 +11,10 @@ cd $HOME # go to the home directory
 echo $BASH # print bash variable
 echo $PATH # print the contents of the environment variable to the screen
 cd /usr/local # move from your current folder to the system folder called /usr/local
+ls -lai # List all files (including hidden) in long format with inode numbers
+ls -l -a -i # Exactly the same as above; flags can be written individually
+man ls  # Open the manual page to see every possible option for the ls command
+ls --all # Show all files, including hidden ones starting with a dot (.)
+ls -a # The short version of --all; shows hidden files in a simple list
+.  # actual directory
+.. # father directory
