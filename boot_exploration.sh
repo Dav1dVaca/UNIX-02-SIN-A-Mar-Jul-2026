062c8130741b043ade1e5b820ca5d76133a6e35a
Dav1dVaca <davacagu@uide.edu.ec>
2026-04-16 13:15:54 +0000

    depth

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -2,3 +2,9 @@ ls -l -a -h # It is the "complete package" for inspecting a directory
 ls -lah # It is the "complete package" for inspecting a directory
 mkdir -- -rf # Create the "-rf" directory
 rmdir -- -rf # Remove the "-rf" directory
+--depth <depth>
+#Create a shallow clone with a history truncated to the specified number of commits. Implies
+#--single-branch unless --no-single-branch is given to fetch the histories near the tips of all branches.
+#If you want to clone submodules shallowly, also pass --shallow-submodules.
+# n to go up
+# N to go down
