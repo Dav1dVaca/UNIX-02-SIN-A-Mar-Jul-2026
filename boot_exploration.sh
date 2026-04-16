f4a93ade4e50dfc03939071d5519c4627b24d64f
Dav1dVaca <davacagu@uide.edu.ec>
2026-04-16 12:53:26 +0000

    rf directory

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -1,3 +1,4 @@
 ls -l -a -h # It is the "complete package" for inspecting a directory
 ls -lah # It is the "complete package" for inspecting a directory
 mkdir -- -rf # Create the "-rf" directory
+rmdir -- -rf # Remove the "-rf" directory
