8101433d90cea411f63594dfbd4b006dffcc12ec
Dav1dVaca <davacagu@uide.edu.ec>
2026-04-24 16:37:53 +0000

    fix problema 2: archivos movidos y renombrados

---
+++ evaluation/history.sh
@@ -0,0 +1,29 @@
+#problema 1 
+1  git remote -v
+    2  git fetch upstream
+    3  git checkout -b eval_p2_1_g2 upstream/eval_p2_1_g2
+    4  git push -u origin eval_p2_1_g2
+    5  sudo apt update
+    6  sudo apt install git-lfs
+    7  git lfs install
+    8  git push -u origin eval_p2_1_g2
+    9  mv docs registros
+   10  pwd
+   11  cd kepler/
+   12  cd /kepler
+   13  cd /evaluation/kepler
+   14  cd /evaluation/kepler/
+   15  cd evaluation/
+   16  cd kepler/
+   17  ls -la
+   18  mv docs registros
+   19  mkdir plantillas
+   20  ls -la
+   21  history
+
+#PROBLEMA 2
+ mv sensores.log registros/
+   23  mv ajustes.old ajustes.conf
+   24  ls
+   25  ls registros/
+#PROBLEMA 3
