50381fe38b18c569803e57074d0c29d8af467f28
Dav1dVaca <davacagu@uide.edu.ec>
2026-05-28 12:55:20 +0000

    newgrp

--- security2.zsh
+++ security2.zsh
@@ -4,3 +4,9 @@ id -gn # only the name of the main group
 #create an achive and see wich group inherits
 touch ~/test_grupo_heredado.txt 
 ls -la ~/test_grupo_heredado.txt # the group is the principal group of the user 
+# see actual group
+id -gn
+echo "Grupo actual:$(id -gn)"
+# create an achive before the newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
