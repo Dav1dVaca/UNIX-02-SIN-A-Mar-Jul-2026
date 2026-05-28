c170f03761170b6f2e8af3bc5de5d19db7842698
Dav1dVaca <davacagu@uide.edu.ec>
2026-05-28 13:45:33 +0000

    compare groups

--- security2.zsh
+++ security2.zsh
@@ -14,3 +14,26 @@ groupadd desarrolladores # create group "desarrolladores"
 newgrp desarrolladores # change to group "desarrolladores"
 id -gn # verify the active group change
 echo "Nuevo grupo activo:$(id -gn)"
+# create an achive inside the subshell
+touch ~/dentro_de_newgrp.txt
+ls -la ~/dentro_de_newgrp.txt
+# the group now is "desarrolladores"
+mkdir -p ~/proyecto_dev/src # create a directory
+ls -la ~/
+# proyecto_dev/ tiene grupo'desarrolladores'
+# exit to the subshell of newgrp
+exit
+#Verify we come back to main group
+id -gn
+echo "Grupo restaurado: $(id- gn)"
+# compare the both achives
+ls -la ~/antes_de_newgrp.txt ~/ dentro_de_newgrp.txt
+#create a group with password
+groupadd grupo_restringido
+gpasswd grupo_restringido # the system will ask a password for the group
+# a user that is not on the group, can join temporaly if knows the password
+newgrp grupo_restringido
+# the system ask the password of the group
+# if the password is rigth, it join temporaly
+id -gn
+exit # when exit, lose the temporaly membership
