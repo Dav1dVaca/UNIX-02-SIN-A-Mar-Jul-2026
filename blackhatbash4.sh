22c1e08dd4467eea1424c70d5e5c99e5d24d3c5b
Dav1dVaca <davacagu@uide.edu.ec>
2026-06-04 13:34:55 +0000

    math expressions

--- blackhatbash4.sh
+++ blackhatbash4.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-PUBLISHER="No Starch Press"    # Define global variable
-
-print_name(){                  # Define function
-    local name                 # Declare local variable
-    name="Black Hat Bash"      # Assign book title
-    echo "${name} by ${PUBLISHER}"  # Print title and publisher
-}
-
-print_name                     # Call function
-
-echo "Variable ${name} will not be printed because it is a local variable."  # Demonstrate local scope
+let result="4 * 5"    # Perform arithmetic operation
+echo ${result}        # Display result
+result=$((5 * 5))   # Perform arithmetic operation
+echo ${result}      # Display result
+result=$(expr 5 + 505)  # Calculate expression
+echo ${result}          # Display result
