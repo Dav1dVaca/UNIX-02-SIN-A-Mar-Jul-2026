d320992e098e8b009295eeb994f931039e01aab6
Dav1dVaca <davacagu@uide.edu.ec>
2026-06-18 12:52:44 +0000

    pront columns $

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -6,3 +6,7 @@ ps | grep TTY
 ps | grep -i tty # -i ignore
 grep -v "35.237.4.214" log.txt # -v show all the lines that do not contain the ip address
 grep -o "35.237.4.214" log.txt # -o only show the matched part of the line
+awk '{print $1}' log.txt # print the first column of log.txt
+awk '{print $1,$2,$3}' log.txt # print the three first columns of the log.txt
+awk '{print $2}' log.txt # print the second column of log.txt
+awk '{print $3}' log.txt # print the third column of log.txt
